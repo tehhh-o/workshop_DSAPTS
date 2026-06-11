6afcabed3cab1354c61f6b737bb7c61045e2324a
aqilH-S <aqilhariz.z99@gmail.com>
2026-06-11 13:31:53 +0800

    Changed advisor sidebar
    also logout arrow is wrong direction

--- advisor/components/topbar-advisor.php
+++
@@ -1,8 +0,0 @@
- <header class="topbar">
-     <div style="width: 120px;"></div>
-     <div class="title"><?php echo isset($pageTitle) ? $pageTitle : "Advisor Dashboard"; ?></div>
-     <div class="user">
-         <span>Advisor -</span>
-         <span class="avatar">👤</span>
-     </div>
- </header>
