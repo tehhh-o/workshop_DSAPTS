893e5c0ef32a935f1e12ee0711d7b88b8a716779
Zyy <hakim.eze@gmail.com>
2026-06-20 07:30:04 +0800

    student login D32155320 password student123 baru boleh masuk

--- models/functions.php
+++ models/functions.php
@@ -5,12 +5,12 @@ include("../database/connection.php");
 // - getCount($conn, $table)                               // for admin dashboard count
 // - getAll($conn, $table)                                 // to get all data from a table except user(admin,advisor,student) and student_subject as these require JOIN
 // - getUserById($conn, $table, $idColumn, $user_id)       // for user profile 
-// - updateUserField($conn, $userId, $field, $value)       // to update a single user profile detail (phone,address,email)
+// - updateUserField($conn, $user_Id, $field, $value)       // to update a single user profile detail (phone,address,email)
 // - deleteUser($conn, $table, $user_id)                   // to delete a user(admin,advisor,student)
 // - searchUserByName($conn, $table, $keyword)             // to search user (admin, advisor, student)
 // - getAllUser($conn, $table)                             // to get all personal detail for all user
-// - getStudentSubjects($conn, $userId)                    // get all subject for a student for all sem
-// - getStudentSubjectsBySemester($conn, $userId, $semId)  // get all subject for a student for a specific sem
+// - getStudentSubjects($conn, $user_Id)                    // get all subject for a student for all sem
+// - getStudentSubjectsBySemester($conn, $user_Id, $semId)  // get all subject for a student for a specific sem
 // - calculateGPA($subjects)                               // to calculate the gpa for a sem or cgpa for all sem, pass $subjects based on type of getStudentSubject called
 
 
@@ -124,7 +124,7 @@ function getStudentSubjects($conn, $userId) // get all subject for a student for
         SELECT student_subject.*, subject.*, semester.*
         FROM student_subject
         INNER JOIN subject ON student_subject.subject_id = subject.subject_id
-        INNER JOIN semester ON student_subject.sem_id = semester.sem_id
+        INNER JOIN semester ON student_subject.semester_id = semester.semester_id
         WHERE student_subject.user_id = '$userId'
     ";
 
@@ -144,9 +144,9 @@ function getStudentSubjectsBySemester($conn, $userId, $semId) // get all subject
         SELECT student_subject.*, subject.*, semester.*
         FROM student_subject
         INNER JOIN subject ON student_subject.subject_id = subject.subject_id
-        INNER JOIN semester ON student_subject.sem_id = semester.sem_id
+        INNER JOIN semester ON student_subject.semester_id = semester.semester_id
         WHERE student_subject.user_id = '$userId'
-        AND student_subject.sem_id = '$semId'
+        AND student_subject.semester_id = '$semId'
     ";
 
     $result = $conn->query($sql);
@@ -196,4 +196,70 @@ function calculateGPA($subjects)  // to calculate the gpa for a sem or cgpa for 
     }
 
     return round($totalPoints / $totalCredits, 2);
+}
+
+function searchsubject($conn, $table, $keyword) // to search subject uisng subject name or or id(student)
+{
+    $sql = "
+        SELECT $table.*, subject.*
+        FROM $table
+        INNER JOIN subject ON $table.subject_id = subject.subject_id
+        WHERE subject.subject_name LIKE '%$keyword%'
+        OR subject.subject_id LIKE '%$keyword%'
+    ";
+    $result = $conn->query($sql);
+    $data = [];
+
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+
+    return $data;
+}
+
+function getStudentByLoginId($conn, $login_id) // get student full profile by session login_id
+{
+    $result = $conn->query("
+        SELECT student.*, user.*
+        FROM student
+        INNER JOIN user ON student.user_id = user.user_id
+        WHERE user.login_id = '$login_id'
+        LIMIT 1
+    ");
+    return $result ? $result->fetch_assoc() : null;
+}
+
+
+function getAllSemesters($conn) // get all semesters for dropdown
+{
+    $result = $conn->query("SELECT * FROM semester ORDER BY semester_id ASC");
+    $data = [];
+ 
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+ 
+    return $data;
+}
+
+function getAlertsByUser($conn, $user_id) // get all alerts for a specific student
+{
+    $sql = "
+        SELECT *
+        FROM alert
+        WHERE user_id = '$user_id'
+        ORDER BY date_sent DESC
+    ";
+    $result = $conn->query($sql);
+    $data = [];
+ 
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+ 
+    return $data;
 }
