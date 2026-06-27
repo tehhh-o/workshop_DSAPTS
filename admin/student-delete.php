79332a8f92bd51499ec75f07eb7eb9576900854e
xyarkokaze <asyrafersyad@gmail.com>
2026-06-27 23:39:36 +0800

    delete button has been made horhor
    kinda rosak but er cuba try test

--- admin/student-delete.php
+++ admin/student-delete.php
@@ -13,40 +13,40 @@
 
 <body class="page-body main-gradient-bg">
     <?php
-    $activePage = 'advisor';
+    $activePage = 'student';
     include("components/sidebar-admin.php");
     include("../models/functions.php");
 
     if (!isset($_GET['id'])) {
-    header("Location: advisor.php");
+    header("Location: student.php");
     exit;
 }
 
     $user_id = $_GET['id'];
 
     if (isset($_POST['confirm_delete'])) {
-        $success = deleteUser($conn, 'advisor', $user_id);
+        $success = deleteUser($conn, 'student', $user_id);
         if ($success) {
-            header("Location: advisor.php?deleted=1");
+            header("Location: student.php?deleted=1");
         } else {
-            header("Location: advisor.php?deleted=0");
+            header("Location: student.php?deleted=0");
         }
         exit;
     }
 
-    $advisor = getUserById($conn, 'advisor', 'advisor.user_id', $user_id);
-        if (!$advisor) {
-        header("Location: advisor.php");
+    $student = getUserById($conn, 'student', 'student.user_id', $user_id);
+        if (!$student) {
+        header("Location: student.php");
         exit;
     }
 ?>
     <main class="main-content main-rounded">
         <div class="confirm-box">
-            <h2>🗑 Delete Advisor</h2>
-            <p>Are you sure you want to delete<br><strong><?= $advisor['name'] ?></strong>?<br><br>This action cannot be undone.</p>
+            <h2>🗑 Delete Student</h2>
+            <p>Are you sure you want to delete<br><strong><?= $student['name'] ?></strong>?<br><br>This action cannot be undone.</p>
             <div class="btn-row">
-                <a class="btn-cancel" href="advisor.php">Cancel</a>
-                <form method="POST" action="advisor-delete.php?id=<?= $user_id ?>">
+                <a class="btn-cancel" href="student.php">Cancel</a>
+                <form method="POST" action="student-delete.php?id=<?= $user_id ?>">
                     <button class="btn-delete" name="confirm_delete" type="submit">Delete</button>
                 </form>
             </div>
